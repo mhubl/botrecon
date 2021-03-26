eb88483f37febca0c5f3c8ec30f49694a004ef3e
mhubl <hubala.mateusz@gmail.com>
2021-03-26 20:34:48 +0100

    Prepared basic setup files

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from botrecon import __version__
+
+
+with open('README.md', 'r') as fp:
+    LONG_DESCRIPTION = fp.read()
+
+setup(
+    name='botrecon',
+    version=__version__,
+    description=('Botrecon uses machine learning to locate hosts infected with'
+                 'botnets in your network.'),
+    long_description_content_type='text/markdown',
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    package_data={'botrecon': ['models/*']},
+    include_package_data=True,
+    install_requires=[
+        'Click',
+        'pandas',
+        'numpy',
+        'scikit-learn==0.24.1'
+    ],
+    entry_points="""
+        [console_scripts]
+        botrecon = botrecon.cli:botrecon
+    """,
+)
