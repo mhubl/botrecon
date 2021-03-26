b4760e0beed2af9afb7e727c4092459986851306
mhubl <hubala.mateusz@gmail.com>
2021-03-26 20:38:04 +0100

    Added __init__ and __main__

---
+++ botrecon/__init__.py
@@ -0,0 +1,7 @@
+from .data import get_data, Data
+from .predictions import get_predictions
+from .ip import IPEntity
+from .output import handle_output
+from .cli import botrecon
+
+__version__ = '0.0.1.dev'
---
+++ botrecon/__main__.py
@@ -0,0 +1,3 @@
+from botrecon import botrecon
+
+botrecon()
